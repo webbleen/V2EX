13aa924a9da3109e9c5238262b321ae2deaa9737
BUNHIN.RI <xri.bunhin@contract.isid.co.jp>
2020-03-12 11:57:38 +0900

    add APIRequest

--- V2EX/Libraries/APIRequest.swift
+++ V2EX/Libraries/APIRequest.swift
@@ -14,4 +14,71 @@ typealias APICallback = (APICallbackResponse, JSON?) -> Void
 
 class APIRequest {
     
+    static fileprivate var _shared = APIRequest()
+    
+    static var share: APIRequest {
+        return _shared
+    }
+    
+    fileprivate lazy var manager: SessionManager = {
+        let configuration = URLSessionConfiguration.default
+        configuration.httpAdditionalHeaders = SessionManager.defaultHTTPHeaders
+        configuration.timeoutIntervalForRequest = 5
+        configuration.timeoutIntervalForResource = 5
+        
+        let ret = SessionManager(configuration: configuration)
+        ret.adapter = self
+        return ret
+    }()
+    
+    var accessToken: String?
+    
+    fileprivate func _request(_ method: HTTPMethod, path: String, parameters: [String: AnyObject]?, callback: @escaping APICallback) {
+        manager.request("\(ROOT_URL)\(path)", method: method, parameters: parameters).responseData { response in
+            switch response.result {
+            case .success(_):
+                let statusCode = response.response!.statusCode
+                log.debug([method, path, statusCode])
+                if statusCode == 401 {
+                    return //logout
+                }
+                var result: JSON? = nil
+                if let data = response.data {
+                    do {
+                        try result = JSON(data: data)
+                    } catch { }
+                }
+                callback(response, result)
+                break
+            case .failure(let error):
+                log.error([method, path, error])
+                callback(response, nil)
+                break
+            }
+        }
+    }
+    
+    func get(_ path: String, parameters: [String: AnyObject]?, callback: @escaping APICallback) {
+        _request(.get, path: path, parameters: parameters, callback: callback)
+    }
+    
+    func post(_ path: String, parameters: [String: AnyObject]?, callback: @escaping APICallback) {
+        _request(.post, path: path, parameters: parameters, callback: callback)
+    }
+    
+    func delete(_ path: String, parameters: [String: AnyObject]?, callback: @escaping APICallback) {
+        _request(.delete, path: path, parameters: parameters, callback: callback)
+    }
+}
+
+extension APIRequest: RequestAdapter {
+    func adapt(_ urlRequest: URLRequest) throws -> URLRequest {
+        var urlRequest = urlRequest
+        
+        if let accessToken = accessToken, let url = urlRequest.url, url.absoluteString.hasPrefix(ROOT_URL) {
+            urlRequest.setValue("Bearer " + accessToken, forHTTPHeaderField: "Authorization")
+        }
+        
+        return urlRequest
+    }
 }
